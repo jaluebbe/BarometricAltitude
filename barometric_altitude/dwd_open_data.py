721a23f91745e87b251ec6404e24db367fbd3786
jannis <jluebbe@uos.de>
2021-08-12 00:45:37 +0200

    removed 10 minutes data.

--- barometric_altitude/dwd_open_data.py
+++ barometric_altitude/dwd_open_data.py
@@ -22,9 +22,6 @@ pressure_hourly_file_re = re.compile(
 temperature_hourly_file_re = re.compile(
     "(?P<file_name>stundenwerte_TU_(?P<station_id>[0-9]{5})_(?:akt|(?:[0-9]{8}_[0-9]{8}_hist)).zip)</a>"
 )
-ten_minutes_file_re = re.compile(
-    "(?P<file_name>10minutenwerte_TU_(?P<station_id>[0-9]{5})_(?:now|akt|(?:[0-9]{8}_[0-9]{8}_hist)).zip)</a>"
-)
 
 
 @timeit
@@ -121,62 +118,6 @@ def get_hourly_stations(date, lat: float, lon: float):
 
 
 @timeit
-def get_10_minutes_stations(date, lat: float, lon: float):
-    selected_date = arrow.get(date)
-    today = arrow.utcnow().floor("day")
-    yesterday = today.shift(days=-1)
-    stations = []
-    if arrow.get(date) < yesterday.shift(days=-500):
-        category = "historical"
-        stations_file = "zehn_min_tu_Beschreibung_Stationen.txt"
-    elif arrow.get(date) < today:
-        category = "recent"
-        stations_file = "zehn_min_tu_Beschreibung_Stationen.txt"
-    else:
-        category = "now"
-        stations_file = "zehn_now_tu_Beschreibung_Stationen.txt"
-    url = (
-        "https://opendata.dwd.de/climate_environment/CDC/observations_germany"
-        f"/climate/10_minutes/air_temperature/{category}/"
-    )
-    stations_response = requests.get(url + stations_file)
-    if not stations_response.status_code == 200:
-        logging.warning("no valid response from server")
-        return []
-    files_response = requests.get(url)
-    if not files_response.status_code == 200:
-        logging.warning("no valid response from server")
-        return []
-    file_names = {
-        _x["station_id"]: _x["file_name"]
-        for _x in ten_minutes_file_re.finditer(files_response.text)
-    }
-    selected_location = eV.LatLon(lat, lon)
-    for _line in stations_response.text.splitlines():
-        _match = station_re.match(_line)
-        if _match is not None:
-            _station_data = _match.groupdict()
-            _from = arrow.get(_station_data["from"])
-            if _from > selected_date:
-                continue
-            _until = arrow.get(_station_data["until"])
-            if _until < selected_date.floor("day"):
-                continue
-            _station_id = _station_data["station_id"]
-            if _station_id not in file_names:
-                continue
-            _station_data["file_name"] = url + file_names[_station_id]
-            _station_location = eV.LatLon(
-                _station_data["lat"], _station_data["lon"]
-            )
-            _distance = selected_location.distanceTo(_station_location)
-            _station_data["distance"] = round(_distance)
-            stations.append(_station_data)
-    sorted_stations = sorted(stations, key=itemgetter("distance"))
-    return {"category": category, "stations": sorted_stations}
-
-
-@timeit
 def get_nearest_hourly_data(date, lat, lon, as_dataframe=False):
     hourly_stations = get_hourly_stations(date, lat, lon)
     if len(hourly_stations) == 0:
@@ -220,43 +161,6 @@ def get_nearest_hourly_data(date, lat, lon, as_dataframe=False):
     }
 
 
-@timeit
-def get_nearest_10_minutes_data(date, lat, lon, as_dataframe=False):
-    ten_minutes_stations = get_10_minutes_stations(date, lat, lon)
-    if len(ten_minutes_stations) == 0:
-        logging.warning("no suitable stations found.")
-        return None
-    nearest_station = ten_minutes_stations["stations"][0]
-    data = unpack_zipped_data_from_url(
-        nearest_station["file_name"], "produkt_zehn_"
-    )
-    data.MESS_DATUM = pd.to_datetime(data.MESS_DATUM, format="%Y%m%d%H%M")
-    data["utc"] = (
-        (data.MESS_DATUM - dt.datetime(1970, 1, 1))
-        .dt.total_seconds()
-        .astype(int)
-    )
-    data.set_index("MESS_DATUM", inplace=True)
-    data.rename(
-        columns={
-            "PP_10": "pressure",
-            "TT_10": "temperature",
-            "RF_10": "humidity",
-        },
-        inplace=True,
-    )
-    data.drop(columns=["QN", "TD_10", "TM5_10"], inplace=True, errors="ignore")
-    if not as_dataframe:
-        output_data = data.to_dict("records")
-    else:
-        output_data = data.to_dict("records")
-    return {
-        "category": ten_minutes_stations["category"],
-        "station": nearest_station,
-        "data": output_data,
-    }
-
-
 if __name__ == "__main__":
     hourly_stations = get_hourly_stations(
         date="20210804T1849", lat=52.52, lon=7.30
@@ -268,22 +172,7 @@ if __name__ == "__main__":
             f"{_station['station_name']}, {_station['pressure_file_name']}, "
             f"{_station['temperature_file_name']}"
         )
-    ten_minutes_stations = get_10_minutes_stations(
-        date="20210808T1349", lat=52.52, lon=7.30
-    )
-    print(f"10_minutes ({ten_minutes_stations['category']}):")
-    for _station in ten_minutes_stations["stations"][:6]:
-        print(
-            f"{_station['distance']/1e3:.1f}km distance to "
-            f"{_station['station_name']}, {_station['file_name']}"
-        )
     data = get_nearest_hourly_data(date="20210804T1849", lat=52.52, lon=7.30)
     print(f"downloaded nearest hourly data for {data['station']}.")
-    print(f"first entry: {data['data'][0]}")
-    print(f"last entry: {data['data'][-1]}")
-    data = get_nearest_10_minutes_data(
-        date="20210804T1849", lat=52.52, lon=7.30
-    )
-    print(f"downloaded nearest 10 minutes data for {data['station']}.")
     print(f"first entry: {data['data'][0]}")
     print(f"last entry: {data['data'][-1]}")
